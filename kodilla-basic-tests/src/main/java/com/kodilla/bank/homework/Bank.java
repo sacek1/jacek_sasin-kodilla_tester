26493cef636d3ecf46da11a1cedd23441c2bfe31
Jacek Sasin <jaceksasin@gmail.com>
2020-06-24 19:04:55 +0200

    3.10 cashmashine_corrected.homework

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -2,92 +2,27 @@ package com.kodilla.bank.homework;
 
 public class Bank {
 
-    private CashMachine cM1;
-    private CashMachine cM2;
-    private CashMachine cM3;
+    private CashMachine[] cashMachines;
 
-    public Bank(){
-        this.cM1 = new CashMachine();
-        this.cM2 = new CashMachine();
-        this.cM3 = new CashMachine();
+    public Bank(CashMachine[] cashMachines) {
+        this.cashMachines = cashMachines;
     }
 
-
-    public void depositCM1(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM1.deposit(value);
-        }
-    }
-
-    public void depositCM2(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM2.deposit(value);
-        }
-    }
-
-    public void depositCM3(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM3.deposit(value);
-        }
-    }
-
-    public void withdrawalCM1(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM1.withdrawal(value);
-        }
-    }
-
-    public void withdrawalCM2(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM2.withdrawal(value);
-        }
-    }
-
-    public void withdrawalCM3(int value) {
-        if (value > 0 && value <= 4000 && value%10 == 0) {
-            this.cM3.withdrawal(value);
-        }
-    }
-
-    public int getCM1Balance() {
-        return this.cM1.getBalance();
-    }
-
-    public int getCM2Balance() {
-        return this.cM2.getBalance();
-    }
-
-    public int getCM3Balance() {
-        return this.cM3.getBalance();
-    }
 
     public int getBalanceAll() {
-        int balanceAll = this.cM1.getBalance() + this.cM2.getBalance() + this.cM3.getBalance();
-        return balanceAll;
+        int s = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            s += cashMachines[i].getBalance();
+        }
+        return s;
     }
 
     public int getNumberOfDeposits() {
-        int numberOfDeposits = this.cM1.getDepositNumber() + this.cM2.getDepositNumber() + this.cM3.getDepositNumber();
-        return numberOfDeposits;
-    }
-
-    public int getNumberOfWithdrawals() {
-        int numberOfWithdrawals = this.cM1.getWithdrawalNumber() + this.cM2.getWithdrawalNumber() + this.cM3.getWithdrawalNumber();
-        return numberOfWithdrawals;
-    }
-
-    public double getDepositAverage() {
-        double depositSum = this.cM1.getDepositSum() + this.cM2.getDepositSum() + this.cM3.getDepositSum();
-        double numberOfDeposits = this.cM1.getDepositNumber() + this.cM2.getDepositNumber() + this.cM3.getDepositNumber();
-        double depositAverage = depositSum/numberOfDeposits;
-        return depositAverage;
-    }
-
-    public double getWithdrawalAverage() {
-        double withdrawalSum = this.cM1.getWithdrawalSum() + this.cM2.getWithdrawalSum() + this.cM3.getWithdrawalSum();
-        double numberOfWithdrawals = this.cM1.getWithdrawalNumber() + this.cM2.getWithdrawalNumber() + this.cM3.getWithdrawalNumber();
-        double withdrawalAverage = withdrawalSum/numberOfWithdrawals;
-        return withdrawalAverage;
+        int s = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            s += cashMachines[i].getDepositNumber();
+        }
+        return s;
     }
 
 }
--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
@@ -8,172 +8,31 @@ public class BankTestSuite {
 
     @Test
     public void shouldPostOnlyGoodValuesOnCM1() {
-        Bank bank = new Bank();
-        bank.depositCM1(50);
-        bank.depositCM1(150);
-        bank.depositCM1(33);
-        bank.depositCM1(4010);
-        bank.depositCM1(1);
-        assertEquals(200, bank.getCM1Balance());
+        // given
+        CashMachine c1  = new CashMachine();
+        fillInCashMachine(c1, new int[]{50, 150, 33, 4010, 1});
+        // when
+        Bank bank = new Bank(new CashMachine[]{c1});
+        // then
+        assertEquals(200, bank.getBalanceAll());
     }
 
     @Test
-    public void shouldPostOnlyGoodValuesOnCM2() {
-        Bank bank = new Bank();
-        bank.depositCM2(300);
-        bank.depositCM2(1500);
-        bank.depositCM2(330);
-        bank.depositCM2(4050);
-        bank.depositCM2(4);
-        assertEquals(2130, bank.getCM2Balance());
+    public void shouldPostValuesOnTwoCashMachines() {
+        // given
+        CashMachine c1  = new CashMachine();
+        fillInCashMachine(c1, new int[]{50, 150, 33, 4010, 1});
+        CashMachine c2  = new CashMachine();
+        fillInCashMachine(c2, new int[]{5, 15, 3, 40, 1});
+        // when
+        Bank bank = new Bank(new CashMachine[]{c1, c2});
+        // then
+        assertEquals(240, bank.getBalanceAll());
     }
 
-    @Test
-    public void shouldPostOnlyGoodValuesOnCM3() {
-        Bank bank = new Bank();
-        bank.depositCM3(20);
-        bank.depositCM3(10);
-        bank.depositCM3(11);
-        bank.depositCM3(3999);
-        bank.depositCM3(8);
-        assertEquals(30, bank.getCM3Balance());
-    }
-
-    @Test
-    public void shouldPayOnlyGoodValuesOnCM1() {
-        Bank bank = new Bank();
-        bank.withdrawalCM1(50);
-        bank.withdrawalCM1(150);
-        bank.withdrawalCM1(33);
-        bank.withdrawalCM1(4010);
-        bank.withdrawalCM1(1);
-        assertEquals(-200, bank.getCM1Balance());
-    }
-
-    @Test
-    public void shouldPayOnlyGoodValuesOnCM2() {
-        Bank bank = new Bank();
-        bank.withdrawalCM2(300);
-        bank.withdrawalCM2(1500);
-        bank.withdrawalCM2(330);
-        bank.withdrawalCM2(4050);
-        bank.withdrawalCM2(4);
-        assertEquals(-2130, bank.getCM2Balance());
-    }
-
-    @Test
-    public void shouldPayOnlyGoodValuesOnCM3() {
-        Bank bank = new Bank();
-        bank.withdrawalCM3(20);
-        bank.withdrawalCM3(10);
-        bank.withdrawalCM3(11);
-        bank.withdrawalCM3(3999);
-        bank.withdrawalCM3(8);
-        assertEquals(-30, bank.getCM3Balance());
-    }
-
-    @Test
-    public void shouldCalculateTotalBalance() {
-        Bank bank = new Bank();
-        bank.depositCM1(50);
-        bank.withdrawalCM1(150);
-        bank.depositCM1(33);
-        bank.depositCM1(4010);
-        bank.withdrawalCM1(1);
-        bank.depositCM2(300);
-        bank.withdrawalCM2(1500);
-        bank.depositCM2(330);
-        bank.depositCM2(4050);
-        bank.depositCM2(4);
-        bank.withdrawalCM3(20);
-        bank.depositCM3(10);
-        bank.depositCM3(11);
-        bank.depositCM3(3999);
-        bank.withdrawalCM3(8);
-        assertEquals(-980, bank.getBalanceAll());
-    }
-
-    @Test
-    public void shouldCalculateNumberOfDeposits() {
-        Bank bank = new Bank();
-        bank.depositCM1(50);
-        bank.depositCM1(150);
-        bank.depositCM1(33);
-        bank.depositCM1(4010);
-        bank.depositCM1(1);
-        bank.depositCM2(300);
-        bank.depositCM2(1500);
-        bank.depositCM2(330);
-        bank.depositCM2(4050);
-        bank.depositCM2(4);
-        bank.depositCM2(20);
-        bank.depositCM3(10);
-        bank.depositCM3(11);
-        bank.depositCM3(3999);
-        bank.depositCM3(8);
-        assertEquals(7, bank.getNumberOfDeposits());
-    }
-
-    @Test
-    public void shouldCalculateNumberOfWithdrawals() {
-        Bank bank = new Bank();
-        bank.withdrawalCM1(50);
-        bank.withdrawalCM1(150);
-        bank.withdrawalCM1(33);
-        bank.withdrawalCM1(4010);
-        bank.withdrawalCM1(1);
-        bank.withdrawalCM2(300);
-        bank.withdrawalCM2(1500);
-        bank.withdrawalCM2(330);
-        bank.withdrawalCM2(4050);
-        bank.withdrawalCM2(4);
-        bank.withdrawalCM2(20);
-        bank.withdrawalCM3(10);
-        bank.withdrawalCM3(11);
-        bank.withdrawalCM3(3999);
-        bank.withdrawalCM3(8);
-        assertEquals(7, bank.getNumberOfWithdrawals());
-    }
-
-    @Test
-    public void shouldCalculateAverageOfDeposits() {
-        Bank bank = new Bank();
-        bank.depositCM1(50);
-        bank.depositCM1(150);
-        bank.depositCM1(33);
-        bank.depositCM1(4010);
-        bank.depositCM1(1);
-        bank.depositCM2(300);
-        bank.depositCM2(1500);
-        bank.depositCM2(330);
-        bank.depositCM2(4050);
-        bank.depositCM2(4);
-        bank.depositCM2(20);
-        bank.depositCM3(10);
-        bank.depositCM3(11);
-        bank.depositCM3(3999);
-        bank.depositCM3(8);
-        assertEquals(337.14, bank.getDepositAverage(), 0.01);
-    }
-
-    @Test
-    public void shouldCalculateAverageOfWithdrawals() {
-        Bank bank = new Bank();
-        bank.withdrawalCM1(50);
-        bank.withdrawalCM1(150);
-        bank.withdrawalCM1(33);
-        bank.withdrawalCM1(4010);
-        bank.withdrawalCM1(1);
-        bank.withdrawalCM2(300);
-        bank.withdrawalCM2(1500);
-        bank.withdrawalCM2(330);
-        bank.withdrawalCM2(4050);
-        bank.withdrawalCM2(4);
-        bank.withdrawalCM2(20);
-        bank.withdrawalCM3(10);
-        bank.withdrawalCM3(11);
-        bank.withdrawalCM3(3999);
-        bank.withdrawalCM3(8);
-        assertEquals(337.14, bank.getWithdrawalAverage(), 0.01);
+    private void fillInCashMachine(CashMachine c, int[] vals) {
+        for (int i = 0; i < vals.length; i++) {
+            c.deposit(vals[i]);
+        }
     }
 }
